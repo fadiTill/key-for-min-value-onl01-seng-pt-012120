19d3f79005c05b5bc122d4e85b9e018c5512771b
fadimata tillman <fadi.tillman@gmail.com>
2020-02-02 22:09:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   ikea = {:chair => 25, :table => 85, :mattress => 450}
-  name_hash.each do {{object, price}
+  name_hash.each do {|object , price|}
     object == "1,2,3"
     price == "a,b,c"
   if   a < b && c
