ce5a436e6136edb95155ff091c9c35afdc4e97e2
fadimata tillman <fadi.tillman@gmail.com>
2020-02-02 20:58:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,8 @@ def key_for_min_value(name_hash)
   ikea = {:chair => 25, :table => 85, :mattress => 450}
  ikea.each do |object, price|
   if  a.price < b.price && c.price
-  puts a.object
+  puts a"#{object}"
 elsif b.price < a.price && c.price
-  puts b.object
+  puts b"#{object}"
+else c.price < a.price && b.price
+  puts c"#{object}
