c84e6f1cb7a779c6eba9f5d521a14c5edef9d039
fadimata tillman <fadi.tillman@gmail.com>
2020-02-02 22:41:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,5 +1,5 @@
 # prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
+require 'pry'# Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
   ikea = {:chair => 25, :table => 85, :mattress => 450}
