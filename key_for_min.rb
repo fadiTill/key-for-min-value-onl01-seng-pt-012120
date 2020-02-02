cceb6fbeb2ac7194ba8986f0d28e054b2028d211
fadimata tillman <fadi.tillman@gmail.com>
2020-02-02 22:39:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,7 @@
 def key_for_min_value(name_hash)
   ikea = {:chair => 25, :table => 85, :mattress => 450}
   name_hash.each do |object,price|
+    binding.pry
   if   price < price && price
   puts name_hash"#{object}"
 else  name_hash == {}
