8336690bac796943668ba75530b1272d9c7a42b8
fadimata tillman <fadi.tillman@gmail.com>
2020-02-02 22:45:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 require 'pry'# Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+  low_key = nil
   name_hash.each do |object,price|
     binding.pry
   if   price < price && price
