56357c174af7fa8aebcd629581a18f3fcd8fa5d2
fadimata tillman <fadi.tillman@gmail.com>
2020-02-02 22:20:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |object,price|
   if   price < price && price
   puts name_hash"#{object}"
-else object == :[]
+else key_for_min_value == key_for_min_value()
   nil
 end
   end
